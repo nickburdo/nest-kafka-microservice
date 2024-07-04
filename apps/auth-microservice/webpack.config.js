779c31db7932e39baa2adc46bd1b8b42b33ff4a1
nickburdo <mykola.burdo@altamira.ai>
2024-07-04 22:25:06 +0300

    auth-microservice fix

--- apps/auth-microservice/webpack.config.js
+++ apps/auth-microservice/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/auth-microservice'),
+    path: join(__dirname, '../../dist/apps/auth-microservice'),
   },
   plugins: [
     new NxAppWebpackPlugin({
