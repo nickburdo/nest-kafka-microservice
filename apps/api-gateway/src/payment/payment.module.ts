ca270e495cd0d2bde4aed89007576000c856aa39
nickburdo <mykola.burdo@altamira.ai>
2024-07-04 20:37:18 +0300

    Create payment API endpoint in the api-gateway

---
+++ apps/api-gateway/src/payment/payment.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PaymentController } from './payment.controller';
+import { PaymentService } from './payment.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PAYMENT_MICROSERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'payment',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'payment-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [PaymentController],
+  providers: [PaymentService],
+})
+export class PaymentModule {}
