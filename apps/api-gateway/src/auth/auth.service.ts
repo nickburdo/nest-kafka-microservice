cfb18d176ae879217dcac68165c3d200a92de5c6
nickburdo <mykola.burdo@altamira.ai>
2024-07-04 19:32:43 +0300

    Implement sign-up endpoint in the api-gateway

--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@nestjs-microservices/shared/dto';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@Inject('AUTH_MICROSERVICE') private readonly authClient: ClientKafka) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    this.authClient.emit('create_user', JSON.stringify(createUserDto));
+  }
+}
